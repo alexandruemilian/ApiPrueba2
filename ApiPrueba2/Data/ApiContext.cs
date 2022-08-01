cf53f8abe3f45423f7edb1e700f373738e1cdc89
alexandru.emilian <alexandru.emilian@20215657-ASP>
2022-08-01 11:30:28 +0200

    Agregar archivos de proyecto.

---
+++ ApiPrueba2/Data/ApiContext.cs
@@ -0,0 +1,16 @@
+﻿using ApiPrueba2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiPrueba2.Data
+{
+    public class ApiContext: DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        public DbSet<Articulo> Articulo { get; set; }
+    }
+}
